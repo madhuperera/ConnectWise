731a2bab36721a6a22b2f4a25b4506345142af89
Madhu @ EIT <madhu@essentialit.co.nz>
2022-11-09 14:53:11 +1300

    ConnectWise Purchase Date Sync Script

--- Configurations/PurchaseDateUpdateFromCSV/SyncPurchaseDatesFromCsv.ps1
+++ Configurations/PurchaseDateUpdateFromCSV/SyncPurchaseDatesFromCsv.ps1
@@ -31,23 +31,99 @@ $Header = @{
 
 
 # Update the code to read the CSV into a variable
-
+$ListOfDevicesToUpdate = Import-Csv -Path $CSV_File_Path
 
 # Update the Code to make changes to the Date Format to what is required by ConnectWise
+function Set-CWDateUS
+{
+    param
+    (        
+        [String] $DateToChange
+    )
 
-$CWM_Config_ID = "" # Change Me please
-$CWM_NewPurchaseDate = "" # Please change Me: "2022-05-11T00:00:00Z"
+    [string] $NewDate = ""
 
-$CWM_GET_URI = "$($CWM_API_Base_URL)/company/configurations/$CWM_Config_ID" # Creating the URI for the given ID
-$CWM_Current_Device = Invoke-restmethod -headers $Header -method GET -uri $CWM_GET_URI # Retrieving the Configuration
+    $tmpCatcher = $DateToChange -split "/"
+    $month = $tmpCatcher[0]
+    if ($month.Length -eq 1)
+    {
+        $month = "0" + $month
+    }
+    $NewDate = -join ($tmpCatcher[2],"-",$month,"-",$tmpCatcher[1])
+    
+    return $($NewDate + "T00:00:00Z")
+}
 
-# Update the code to confirm we are only looking at one configuration
+# New Object to store Duplicate Data
+$DuplicateDevices = @()
 
-# Update the code to match the Date Required to what is currently configured
+[int] $CurrentItem = 1
+$TotalItemCount = $ListOfDevicesToUpdate.count
+foreach ($Device in $ListOfDevicesToUpdate)
+{
+    Write-Host "-------------------------------- $CurrentItem out of $TotalItemCount --------------------------------" -ForegroundColor Green
+    $CWM_Config_ID = $Device.Configuration_RecId
 
-$CWM_PATCH_BODY = '[{"op": "replace", "path": "purchaseDate", "value": "' + $CWM_DeviceWarrantyDate + '"}]' # Operation Set to "replace" the purchase date
-$CWM_PATCH_URI = "$($CWM_API_Base_URL)/company/configurations/$ID" # Creating the URI for the given ID
+    # Checking to see if ther are duplicates
+    if (($ListOfDevicesToUpdate | Where-Object {$_.Configuration_RecId -eq "$($Device.Configuration_RecId)"}).count -gt 1)
+    {
+        Write-Host "Error: Configuration ID: $CWM_Config_ID is a duplicate within CSV" -ForegroundColor Red
 
-$Results = Invoke-restmethod -headers $Header -method PATCH -uri $CWM_PATCH_URI -Body $CWM_PATCH_BODY # Updating the configuration with the new Body
+        $NewDuplicate = @{
+            'Audit_Value' = $Device.Audit_Value;
+            'Date_Purchased' = $Device.Date_Purchased;
+            'Audit_Token' = $Device.Audit_Token;
+            'Config_Name' = $Device.Config_Name;
+            'Configuration_RecId' = $Device.Configuration_RecId
+        }
+        $PSObject = New-Object -TypeName PSObject -Property $NewDuplicate
+        $DuplicateDevices += $PSObject
+    }
+    else
+    {
+        $CWM_NewPurchaseDate = Set-CWDateUS -DateToChange $($Device.Audit_Value)
 
-# update the code to check if the Results match what was in the CSV
+        Write-Host "Configuration ID: $CWM_Config_ID" -ForegroundColor White
+        Write-Host "Configuration Name: $($Device.Config_Name)" -ForegroundColor White
+        Write-Host "Configuration Purchase Date: $($Device.Audit_Value) ----> $CWM_NewPurchaseDate" -ForegroundColor White
+
+        $CWM_GET_URI = "$($CWM_API_Base_URL)/company/configurations/$CWM_Config_ID" # Creating the URI for the given ID
+        $CWM_Current_Device = Invoke-restmethod -headers $Header -method GET -uri $CWM_GET_URI # Retrieving the Configuration
+
+        if ($CWM_Current_Device.count)
+        {
+            Write-Host "Error: We found two devices with the same configuration ID" -ForegroundColor Red
+        }
+        else
+        {
+            [string] $CWM_Current_DevicePDate = ($CWM_Current_Device.purchaseDate -split "T")[0]
+            if ($CWM_Current_DevicePDate -like "*$($CWM_NewPurchaseDate)*" )
+            {
+                Write-Host "Already up to date: $CWM_NewPurchaseDate is as same as $($CWM_Current_Device.purchaseDate)" -ForegroundColor Yellow
+            }
+            else
+            {
+                Write-Host "Configuration will be updated to replace $($CWM_Current_Device.purchaseDate) with $CWM_NewPurchaseDate" -ForegroundColor Red
+                
+                $CWM_PATCH_BODY = '[{"op": "replace", "path": "purchaseDate", "value": "' + $CWM_NewPurchaseDate + '"}]' # Operation Set to "replace" the purchase date
+                $CWM_PATCH_URI = "$($CWM_API_Base_URL)/company/configurations/$CWM_Config_ID" # Creating the URI for the given ID
+
+                try
+                {
+                    $Results = Invoke-restmethod -headers $Header -method PATCH -uri $CWM_PATCH_URI -Body $CWM_PATCH_BODY
+                }
+                catch
+                {
+                    Write-Host "Error: Trying to update" -ForegroundColor Red
+                    Write-Host "$_.Exception"
+                }
+
+                Write-Host "Configuration was successfully updated $($Results.purchaseDate) <--> $CWM_NewPurchaseDate" -ForegroundColor Green
+            }
+        }
+    } 
+    $CurrentItem += 1
+}
+
+Write-Host "-------------------------------- Duplicate Devices --------------------------------" -ForegroundColor Yello
+$DuplicateDevices | Select-Object Audit_Value, Date_Purchased, Audit_Token, Config_Name, Configuration_RecId
